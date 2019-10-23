8a96167abb83768c8cb1c124972f943cd9bc56ec
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-23 14:07:05 +0100

    Sulfuras test for no change in attributes

--- ruby/spec/gilded_rose_spec.rb
+++ ruby/spec/gilded_rose_spec.rb
@@ -39,5 +39,37 @@ describe GildedRose do
       GildedRose.new(items).update_quality
       expect(items[0].quality).to eq(1)
     end
+
+    it 'Brie quality increases by 2 when sell_in negative' do
+      items = [Item.new('Aged Brie', 0, 0)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(2)
+    end
+
+    it 'Brie continues to increase in quality by 2 after negative' do
+      items = [Item.new('Aged Brie', 0, 0)]
+      shop = GildedRose.new(items)
+      shop.update_quality
+      shop.update_quality
+      expect(items[0].quality).to eq(4)
+    end
+
+    it 'quality is never more than 50' do
+      items = [Item.new('Aged Brie', 0, 50)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(50)
+    end
+
+    it 'Sulfuras attributes stay the same' do
+      items = [Item.new('Sulfuras, Hand of Ragnaros', 6, 40)]
+      GildedRose.new(items).update_quality
+      expect(items[0].quality).to eq(40)
+      expect(items[0].sell_in).to eq(6)
+    end
   end
+
+  # describe 'Backstage passes' do
+  #   it ''
+  # end
+
 end
