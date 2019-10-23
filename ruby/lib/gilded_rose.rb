e76734c1c4e4ef1e84e40dd36d928b6cfd91591e
Gabriel Gonzalez <gabokappa@gmail.com>
2019-10-23 17:30:17 +0100

    Refactored to reduce repetition of code

--- ruby/lib/gilded_rose.rb
+++ ruby/lib/gilded_rose.rb
@@ -18,13 +18,13 @@ class GildedRose
       else
         standard_update(item)
       end
+      item.sell_in -= 1 unless item.name == 'Sulfuras, Hand of Ragnaros'
       end
   end
 
   private
 
   def standard_update(item)
-    item.sell_in -= 1
     item.quality -= 1 if item.quality > 0
     item.quality -= 1 if item.sell_in <= 0 && item.quality > 0
   end
@@ -32,7 +32,6 @@ class GildedRose
   def brie_update(item)
     item.quality += 1 if item.sell_in <= 0 && item.quality < 50
     item.quality += 1 if item.quality < 50
-    item.sell_in -= 1
   end
 
   def backstage_update(item)
@@ -40,13 +39,11 @@ class GildedRose
     item.quality += 1 if item.sell_in <= 10 && item.quality < 50
     item.quality += 1 if item.sell_in <= 5 && item.quality < 50
     item.quality = 0 if item.sell_in <= 0
-    item.sell_in -= 1
   end
 
   def conjured_update(item)
     item.quality -= 2
     item.quality -= 2 if item.sell_in <= 0
-    item.sell_in -= 1
     item.quality = 0 if item.quality.negative?
   end
 end
